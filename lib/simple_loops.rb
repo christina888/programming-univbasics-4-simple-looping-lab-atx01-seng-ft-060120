28d75f51cdd306219769cca4d8e90074fcaabd51
Christina Chianelli <cechianelli@gmail.com>
2020-05-21 21:28:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,17 @@
 # Write your methods here
+
+def loop_message_five_times (message)
+  counter = 0 
+  while counter <=5 do
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times (message, num)
+  counter = 0 
+  while counter <=num do
+    puts message 
+    counter += 1 
+  end
+end
